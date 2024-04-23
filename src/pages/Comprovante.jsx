0327806d542c88fbd5bfe7a5707b6c3523fffdac
marconesb <marconebritt@gmail.com>
2024-04-22 23:31:20 -0300

    ..........

--- src/pages/Comprovante.jsx
+++ src/pages/Comprovante.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
 import enreje from '../public/Pix.png';
