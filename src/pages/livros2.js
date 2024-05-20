ab17a7e0383024bfbda4a17e43c2223ae1cfacb1
marconebrito <marcone.brito@elystech.com.br>
2024-05-19 21:56:05 -0300

    Add de livros

--- src/pages/livros2.js
+++ src/pages/livros2.js
@@ -2,12 +2,20 @@ import livro1 from '../images/Caminho Espírita/Caminho Espirita.jpg';
 import livro2 from '../images/Caminho Verdade e Vida/caminho verdade e vida.jpg';
 import livro3 from '../images/Ceifa de Luz/Ceifa de Luz.jpg';
 import livro4 from '../images/Cinquenta Anos depois/Cinquenta anos depois.png';
+import livro5 from '../images/Alguém me tocou/Alguém me tocou.jpg';
+import livro6 from '../images/Alvorada Cristã/Alvorada Cristã.jpg';
+import livro7 from '../images/As dores da Alma/As dores da Alma.jpg';
+import livro8 from '../images/Continuidade/Continuidade.jpg';
 
 export const livros2 = [
     { image: livro1, title: 'Caminho Espírita', description: 'O Caminho Espírita é uma compilação de mensagens que, seguindo as orientações de Emmanuel, funcionam como lembretes valiosos em nossa jornada pelo reino interior da alma.Estas mensagens nos guiam, ajudando-nos a superar dificuldades e...', price: '40,00' },
     { image: livro2, title: 'Caminho, verdade e vida', description: 'Emmanuel extrai de conceitos do Cristo, ou de seus colaboradores diretos, a lição para determinada situação do Espírito nos caminhos da vida de relação. o autor comenta ensinamentos do Evangelho de forma original e atraente, ensinando-nos não apenas a...', price: '60,00' },
     { image: livro3, title: 'Ceifa de luz', description: 'Este livro apresenta um tesouro de mensagens reconfortantes e diretrizes essenciais, lembrando ao leitor a importância de aproveitar plenamente as experiências diárias em busca da iluminação interior, através do cultivo de sentimentos nobres e do trabalho...', price: '60,00' },
     { image: livro4, title: 'Cinquenta anos depois', description: 'Apenas décadas de trabalho árduo e sofrimento podem desfazer os séculos de egoísmo, orgulho e ambição que nos conduziram à injustiça! Após cinquenta anos desde sua encarnação como o senador Publius Lentulus, o Espírito Emmanuel retorna à vida...', price: '60,00' },
+    { image: livro5, title: 'Alguém me tocou', description: 'A Bíblia nos traz as palavras de Jesus, das quais buscamos conforto nos momentos difíceis. No entanto, este livro nos lembra que o Mestre espera mais de nós do que simplesmente sermos consolados por Ele; Ele espera que também O sigamos de...', price: '84,00' },
+    { image: livro6, title: 'Alvorada Cristã', description: 'Através de contos e crônicas breves, o Espírito Neio Lúcio inspira crianças e jovens a cultivarem uma mente saudável, protegendo-se contra sentimentos como raiva e desânimo, e buscando sempre o crescimento moral e o engajamento no bem...', price: '70,00' },
+    { image: livro7, title: 'As dores da alma', description: 'Hammed, por meio das questões presentes em "O Livro dos Espíritos", explora uma série de estados emocionais e padrões comportamentais, como depressão, medo, culpa, mágoa e rigidez, os quais ele denomina "dores da alma"....', price: '35,00' },
+    { image: livro8, title: 'Continuidade', description: 'Se a dificuldade e a provação constituem traços predominantes em seu caminho não te revoltes nem admitas que o desânimo te assopre aos ouvidos alguma sugestão infeliz.    Atente aos deveres que te competem e prossegue em tua jornada renovadora...', price: '70,50' },
     // Adicione os demais livros aqui
 ];
 
--- src/pages/livros4.js
+++ src/pages/livros4.js
@@ -2,12 +2,20 @@ import livro1 from '../images/Contos desta e doutra Vida/Contos desta e doutra V
 import livro2 from '../images/Coragem/Coragem.jpg';
 import livro3 from '../images/Crônicas de Além-Túmulo/Crônicas de Além-Túmulo.jpg';
 import livro4 from '../images/E a vida continua/E a vida continua.jpg';
+import livro5 from '../images/Contos desta e doutra Vida/Contos desta e doutra Vida.jpg';
+import livro6 from '../images/Coragem/Coragem.jpg';
+import livro7 from '../images/Crônicas de Além-Túmulo/Crônicas de Além-Túmulo.jpg';
+import livro8 from '../images/E a vida continua/E a vida continua.jpg';
 
 export const livros4 = [
     { image: livro1, title: 'Contos desta e doutra Vida', description: 'Com narrativas envolventes que retratam exemplos da vida diária, o autor transmite valiosos ensinamentos aos "viajantes da Terra", seja enfrentando pequenos obstáculos, seja refletindo sobre escolhas que conduzam ao sucesso. Em contos simples...', price: '45,00' },
     { image: livro2, title: 'Coragem', description: 'Para uma alma em evolução que ainda somos, este livro pode servir como um guia na formação do homem virtuoso de amanhã. São ensinamentos simples, baseados nos princípios cristãos, que tocam profundamente nossos corações e nos impulsionam a ser pessoas...', price: '40,00' },
     { image: livro3, title: 'Crônicas de Além-Túmulo', description: 'O Espírito Humberto de Campos, por meio da psicografia de Chico Xavier, reúne uma série de elucidativas crônicas, abordando temas diversos como longevidade, paz, verdade e justiça, além de retratar figuras como Sócrates, Charles Richet, os apóstolos Pedro...', price: '60,00' },
     { image: livro4, title: 'E a vida continua', description: 'O Espírito André Luiz narra as histórias de Ernesto e Evelina, dois indivíduos doentes que se encontram às vésperas de cirurgias arriscadas. Ele é um pensador livre e experiente, enquanto ela é uma jovem esposa católica devota. Em breve, descobrirão que a doença...', price: '71,00' },
+    { image: livro5, title: 'Contos desta e doutra Vida', description: 'Com narrativas envolventes que retratam exemplos da vida diária, o autor transmite valiosos ensinamentos aos "viajantes da Terra", seja enfrentando pequenos obstáculos, seja refletindo sobre escolhas que conduzam ao sucesso. Em contos simples...', price: '45,00' },
+    { image: livro6, title: 'Coragem', description: 'Para uma alma em evolução que ainda somos, este livro pode servir como um guia na formação do homem virtuoso de amanhã. São ensinamentos simples, baseados nos princípios cristãos, que tocam profundamente nossos corações e nos impulsionam a ser pessoas...', price: '40,00' },
+    { image: livro7, title: 'Crônicas de Além-Túmulo', description: 'O Espírito Humberto de Campos, por meio da psicografia de Chico Xavier, reúne uma série de elucidativas crônicas, abordando temas diversos como longevidade, paz, verdade e justiça, além de retratar figuras como Sócrates, Charles Richet, os apóstolos Pedro...', price: '60,00' },
+    { image: livro8, title: 'E a vida continua', description: 'O Espírito André Luiz narra as histórias de Ernesto e Evelina, dois indivíduos doentes que se encontram às vésperas de cirurgias arriscadas. Ele é um pensador livre e experiente, enquanto ela é uma jovem esposa católica devota. Em breve, descobrirão que a doença...', price: '71,00' },
     // Adicione os demais livros aqui
 ];
 
