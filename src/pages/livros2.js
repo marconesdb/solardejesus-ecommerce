52bbef5a49a44c25bb7073c186f76654ceedd843
marconebrito <marcone.brito@elystech.com.br>
2024-05-21 23:47:03 -0300

    Add de + Livros

--- src/pages/livros2.js
+++ src/pages/livros2.js
@@ -6,10 +6,10 @@ import livro5 from '../images/Alguém me tocou/Alguém me tocou.jpg';
 import livro6 from '../images/Alvorada Cristã/Alvorada Cristã.jpg';
 import livro7 from '../images/As dores da Alma/As dores da Alma.jpg';
  import livro8 from '../images/Continuidade/Continuidade.jpg';
-// import livro9 from '../images/Caminho Espírita/Caminho Espirita.jpg';
-// import livro10 from '../images/Caminho Verdade e Vida/caminho verdade e vida.jpg';
-// import livro11 from '../images/Ceifa de Luz/Ceifa de Luz.jpg';
-// import livro12 from '../images/Cinquenta Anos depois/Cinquenta anos depois.png';
+ import livro9 from '../images/No Mundo Maior/No Mundo Maior.jpg';
+ import livro10 from '../images/O amigo Jesus/O amigo Jesus.jpg';
+import livro11 from '../images/O Consolador/O Consolador2.jpg';
+import livro12 from '../images/O Espírito da Verdade/O Espírito da Verdade.jpg';
 // import livro13 from '../images/Alguém me tocou/Alguém me tocou.jpg';
 // import livro14 from '../images/Alvorada Cristã/Alvorada Cristã.jpg';
 // import livro15 from '../images/As dores da Alma/As dores da Alma.jpg';
@@ -24,10 +24,10 @@ export const livros2 = [
     { image: livro6, title: 'Alvorada Cristã', description: 'Através de contos e crônicas breves, o Espírito Neio Lúcio inspira crianças e jovens a cultivarem uma mente saudável, protegendo-se contra sentimentos como raiva e desânimo, e buscando sempre o crescimento moral e o engajamento no bem...', price: '70,00' },
     { image: livro7, title: 'As dores da alma', description: 'Hammed, por meio das questões presentes em "O Livro dos Espíritos", explora uma série de estados emocionais e padrões comportamentais, como depressão, medo, culpa, mágoa e rigidez, os quais ele denomina "dores da alma"....', price: '35,00' },
     { image: livro8, title: 'Continuidade', description: 'Se a dificuldade e a provação constituem traços predominantes em seu caminho não te revoltes nem admitas que o desânimo te assopre aos ouvidos alguma sugestão infeliz.    Atente aos deveres que te competem e prossegue em tua jornada renovadora...', price: '70,50' },
-    // { image: livro9, title: 'Caminho Espírita', description: 'O Caminho Espírita é uma compilação de mensagens que, seguindo as orientações de Emmanuel, funcionam como lembretes valiosos em nossa jornada pelo reino interior da alma.Estas mensagens nos guiam, ajudando-nos a superar dificuldades e...', price: '40,00' },
-    // { image: livro10, title: 'Caminho, verdade e vida', description: 'Emmanuel extrai de conceitos do Cristo, ou de seus colaboradores diretos, a lição para determinada situação do Espírito nos caminhos da vida de relação. o autor comenta ensinamentos do Evangelho de forma original e atraente, ensinando-nos não apenas a...', price: '60,00' },
-    // { image: livro11, title: 'Ceifa de luz', description: 'Este livro apresenta um tesouro de mensagens reconfortantes e diretrizes essenciais, lembrando ao leitor a importância de aproveitar plenamente as experiências diárias em busca da iluminação interior, através do cultivo de sentimentos nobres e do trabalho...', price: '60,00' },
-    // { image: livro12, title: 'Cinquenta anos depois', description: 'Apenas décadas de trabalho árduo e sofrimento podem desfazer os séculos de egoísmo, orgulho e ambição que nos conduziram à injustiça! Após cinquenta anos desde sua encarnação como o senador Publius Lentulus, o Espírito Emmanuel retorna à vida...', price: '60,00' },
+    { image: livro9, title: 'No Mundo Maior', description: 'Testemunha de vários casos tratados em planos superiores, o Espírito André Luiz explora as causas do desequilíbrio mental e as possíveis consequências enfrentadas pelos irmãos imersos na insanidade. Questões como suicídio, interrupção da gestação, epilepsia...', price: '67,00' },
+     { image: livro10, title: 'O amigo Jesus', description: 'O leitor perceberá que, mesmo nos momentos mais difíceis, nunca estará desamparado, pois tem Jesus ao seu lado, aquele que conheceu o amor, a dor e todas as experiências humanas, falando diretamente ao seu coração. É suficiente permitir que essa amizade...', price: '67,00' },
+    { image: livro11, title: 'O Consolador', description: 'Estruturada em formato de perguntas e respostas, esta valiosa obra apresenta importantes esclarecimentos sobre a Ciência, a Filosofia e a Religião — os três pilares da Doutrina Espírita. O livro, repleto de ensinamentos espirituais, O Consolador, oferece um vasto material...', price: '55,00' },
+     { image: livro12, title: 'O Espírito da Verdade', description: 'Anália Franco, André Luiz, Bezerra de Menezes, Emmanuel, Eurípedes Barsanulfo, Meimei, e outros guias espirituais se reúnem para discutir os capítulos e ensinamentos de "O Evangelho Segundo o Espiritismo", explorando temas como amor,caridade...', price: '68,00' },
     // { image: livro13, title: 'Alguém me tocou', description: 'A Bíblia nos traz as palavras de Jesus, das quais buscamos conforto nos momentos difíceis. No entanto, este livro nos lembra que o Mestre espera mais de nós do que simplesmente sermos consolados por Ele; Ele espera que também O sigamos de...', price: '84,00' },
     // { image: livro14, title: 'Alvorada Cristã', description: 'Através de contos e crônicas breves, o Espírito Neio Lúcio inspira crianças e jovens a cultivarem uma mente saudável, protegendo-se contra sentimentos como raiva e desânimo, e buscando sempre o crescimento moral e o engajamento no bem...', price: '70,00' },
     // { image: livro15, title: 'As dores da alma', description: 'Hammed, por meio das questões presentes em "O Livro dos Espíritos", explora uma série de estados emocionais e padrões comportamentais, como depressão, medo, culpa, mágoa e rigidez, os quais ele denomina "dores da alma"....', price: '35,00' },
